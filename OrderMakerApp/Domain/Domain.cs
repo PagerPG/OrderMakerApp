8f29cc17b482ee7c47bc91d03fbebaa55a91de44
Piotr Gemziak <piotrgemziak@gmail.com>
2022-12-19 21:58:11 +0100

    Domain

--- OrderMakerApp/Domain/Domain.cs
+++ OrderMakerApp/Domain/Domain.cs
@@ -2,29 +2,53 @@
 {
     public class Order
     {
-        public Guid OrderId { get; set; }
+        public Order(string orderNumber, int clientId)
+        {
+            OrderNumber = orderNumber;
+            ClientId = clientId;
+            OrderLines = new List<OrderLine>();
+        }
+
+        public int OrderId { get; set; }
+
         public string OrderNumber { get; set; }
-        public Guid ClientId { get; set; }
+        public int ClientId { get; set; }
         public List<OrderLine> OrderLines { get; set; }
     }
 
     public class OrderLine
     {
-        public Guid OrderLineId { get; set; }
-        public Guid ProductId { get; set; }
+        public OrderLine(int productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+
+        public int OrderLineId { get; set; }
+        public int ProductId { get; set; }
         public int Quantity { get; set; }
     }
 
     public class Product
     {
-        public Guid ProductId { get; set; }
+        public Product(string productName)
+        {
+            ProductName = productName;
+        }
+
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int Quantity { get; set; }
     }
 
     public class Client
     {
-        public Guid ClientId { get; set; }
+        public Client(string clientName, string email)
+        {
+            ClientName = clientName;
+            Email = email;
+        }
+
+        public int ClientId { get; set; }
         public string ClientName { get; set; }   
         public string Email { get; set; }
     }
